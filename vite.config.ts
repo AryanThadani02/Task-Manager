535a2b3b869082db6a4b164ecd91d408d4142c77
elf22 <39664482-elf22@users.noreply.replit.com>
2025-02-14 09:23:18 +0000

    User checkpoint: Update development server allowed host.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [tailwindcss()],
   server: {
     allowedHosts: [
-      "7314c0be-1177-4d88-8b3e-6ef1123973e1-00-q30xllo7c8xt.pike.replit.dev",
+      "afdbfd35-13c7-4d77-bea7-b2ece0b43a38-00-289wk7jp88beq.pike.replit.dev",
     ],
   },
 });
