26651c8d54d1573edd8651af9ea8a76197170fb7
Aryan Thadani <126406002+AryanThadani02@users.noreply.github.com>
2025-02-15 18:56:46 +0000

    Assistant checkpoint: Added useEffect import
    Assistant generated file changes:
    - src/components/EditTaskModal.tsx: Add useEffect to React imports
    
    ---
    
    User prompt:
    
    Uncaught ReferenceError: useEffect is not defined
        at EditTaskModal (EditTaskModal.tsx:18:3)

--- src/components/EditTaskModal.tsx
+++ src/components/EditTaskModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { modifyTask } from "../redux/taskSlice";
 import { Task } from "../types/Task";
