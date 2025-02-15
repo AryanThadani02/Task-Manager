e667c7657ff095ddd783aa0123c15806af1e21f7
Aryan Thadani <aryanthadani0@gmail.com>
2025-02-16 00:57:18 +0530

    cleanup done

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [tailwindcss()],
   server: {
     allowedHosts: [
-      "5e0569d7-12e4-4dd7-9f41-6fa59f1234d2-00-2nlbsu1g57l2m.pike.replit.dev",
+      "https://task-manager-xktj.vercel.app/",
     ],
   },
 });
